cee591cc0f0186178f78a79c434f8420a37b407a
Denis-Lukwago <dennislukwago44@gmail.com>
2026-09-13 09:52:27 +0300

    Add files via upload

---
+++ Test class 1.py
@@ -0,0 +1,8 @@
+number_1 = int(input("Enter number:"))
+number_2 = int(input("Enter number:"))
+  #Choosing what is greater
+if(number_1>number_2):
+    largest_number = number_1
+else:
+    largest_number = number_2
+    print("The largest of all is:" + largest_number)
